8aaa8a6418146a691e002dea4df8c4d2bf1f0f8a
hyperlolorb@gmail.com <hyperlolorb@gmail.com>
2022-11-12 15:59:09 -0600

    fix: removed purge from config as it is not used by tailwind anymore

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
